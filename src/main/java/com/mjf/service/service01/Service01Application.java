73ac244c2e2d8bb8422bc46dab47292b6cae2d3b
1571221458@qq.com <majiafei>
2018-12-12 18:48:08 +0800

    初次提交

---
+++ src/main/java/com/mjf/service/service01/Service01Application.java
@@ -0,0 +1,14 @@
+package com.mjf.service.service01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Service01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service01Application.class, args);
+    }
+}
